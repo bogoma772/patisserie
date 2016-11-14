ab163bbc1f33059c95c34f40138e5100540eec3a
olgakostyrnaya <olgakostyrnaya@mail.ru>
2016-11-15 00:05:42 +0500

    changes

--- candyshop/candyshopProject/candyshopProject/Form1.cs
+++ candyshop/candyshopProject/candyshopProject/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace candyshopProject
 {
@@ -17,8 +19,46 @@ namespace candyshopProject
             InitializeComponent();
         }
 
+        private MemoryStream getStreamFromString(string str)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(str);
+            stream.Position = 0;
+            return stream;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            label6.Text = "";
+            string pas1 = "abcd";
+            string pas2 = "ab";
+            MemoryStream stream1;
+            stream1 = getStreamFromString(pas1);
+            MemoryStream stream2;
+           stream2 = getStreamFromString(pas2);
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            
+            // This is one implementation of the abstract class SHA1.
+            string res1;
+            string res2;
+
+            sha.ComputeHash(stream1);
+            res1 = Convert.ToBase64String(sha.Hash);
+            sha.ComputeHash(stream2);
+            res2 = Convert.ToBase64String(sha.Hash);
+
+
+            //            res1 = Convert.ToBase64String(sha.ComputeHash(stream1));
+            //          res2 = Convert.ToBase64String(sha.ComputeHash(stream2));
+
+            // bool b1 = res1.Equals(res2);
+
+            label5.Text = res1;
+            label6.Text = res2;
+            
+              // label6.Text = b1.ToString();
+            
 
         }
 
